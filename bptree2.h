b83887d475b5a2d7f9919a9b2a2b2c63ad749ddc
JafetPoco <jpococ@unsa.edu.pe>
2025-07-24 00:22:03 -0500

    Separacion de archivos de B+ tree

---
+++ bptree2.h
@@ -0,0 +1,39 @@
+#ifndef B_PLUS_TREE_H
+#define B_PLUS_TREE_H
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+struct Node {
+  bool isLeaf;
+  vector<int> keys;
+  vector<Node*> children;
+  Node* next;
+  int order;
+
+  Node(bool leaf = false) : isLeaf(leaf), next(nullptr) {}
+};
+
+class BPlusTree {
+  Node* root;
+
+public:
+  BPlusTree(int o) : root(nullptr), order(o) {}
+
+  bool readSerialized(const std::string &serialized);
+  std::string getSerialized() const;
+  
+  void insert(int key);
+  void remove(int key);
+  void print();
+
+private:
+  int insertInternal(Node* node, int key, Node*& newChild);
+  void removeInternal(Node* node, int key);
+};
+
+#endif
